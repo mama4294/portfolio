3bb0677d7ba46f0e08090b71a312a645700401eb
Matthew Malone <mama4294@colorado.edu>
2022-04-30 16:42:25 -0600

    modifies css for better view on large screens

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -2,10 +2,12 @@
 export const About = () => {
   return (
     <section className="p-10 bg-dark" id="about">
+      <div className="md:w-9/12 m-auto">
             <h1 className="text-white text-4xl text-center">About<span className="text-primary">.</span></h1>
-            <div className="m-10">
+            <div className="my-10">
               <p className="text-white">Hi, i'm Matthew Malone, and chemical engineer by training and coding is a passion of mine. I began in industrial automation by programming automated sequences for the food and beverage industry. I built mobile applications and dashboards to report on plant metrics. Since then, I moved into front-end web design and enjoy making clean and functional websites.</p>
             </div>
+      </div>
     </section>
   )
 }
--- src/routes/Skills.jsx
+++ src/routes/Skills.jsx
@@ -3,7 +3,7 @@ export const Skills = () => {
   return (
     <section className="bg-primary p-10" id="skills">
         <h1 className="text-white text-4xl text-center">Skills<span className="text-default">.</span></h1>
-        <div className="md:p-5">
+        <div className="md:p-5 md:w-9/12 m-auto">
           <SkillHeader>Web</SkillHeader>
           <SkillList>
             <SkillItem>HTML5</SkillItem>
