01b962c34e9512109b16b576b3342b82c2e30757
Matthew Malone <mama4294@colorado.edu>
2022-05-03 21:28:14 -0600

    adds scale on hover to project preview component

--- src/routes/Projects.jsx
+++ src/routes/Projects.jsx
@@ -56,9 +56,9 @@ export const Projects = () => {
       picture = pictures[0]
     }
       return (
-          <button className="pointer relative group bg-primary p-4 text-white rounded-md" onClick={handleClick}>
+          <button className="group pointer relative bg-primary p-4 text-white rounded-md" onClick={handleClick}>
               <div className="p-4 ">
-                <img className="rounded-md w-52 h-52" src={picture ? process.env.PUBLIC_URL + picture : example} alt="site" />
+                <img className="rounded-md w-52 h-52 group-hover:scale-110" src={picture ? process.env.PUBLIC_URL + picture : example} alt="site" />
               </div>
               <div className="group-hover:block hidden absolute top-1/3 left-0 right-0 z-10">
                 <p className="bg-muted px-4 py-2 m-2 rounded-md inline-block">Learn More</p>
