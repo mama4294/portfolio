49adc1a6f63c4102dec530754770e7bd16fb3c21
Matthew Malone <mama4294@colorado.edu>
2022-05-08 14:36:25 -0600

    adds hidden form input

--- src/routes/Contact.jsx
+++ src/routes/Contact.jsx
@@ -18,16 +18,28 @@ const {name, email, message} = formFields;
 
 
     return(
-        <section className="bg-light p-10" id="portfolio-contact">
+        <section className="bg-light p-10" id="contact">
             <div className="py-10">
                 <h1 className="text-primary text-4xl text-center">Contact<span className="text-default">.</span></h1>
             </div>
             <div className="w-full md:w-1/2 m-auto">
                 <form 
                     onSubmit="submit" 
-                    name="contact" 
+                    name="portfolio-contact" 
                     method="POST" 
-                    data-netlify="true">
+                    data-netlify="true"
+                    netlify-honeypot="bot-field"
+                    >
+                     
+                  <input
+                    type="hidden"
+                    name="form-name"
+                    value="portfolio-contact"
+                />
+                  <label className="hidden">
+                    Don’t fill this out if you’re human:
+                    <input name="bot-field" />
+                </label>
                 <FormInput type="text" value={name} label="Name" name="name" placeholder="John Doe" onChange={handleChange}/>
                 <FormInput type="email" value={email} label="Email" name="email" placeholder="example@gmail.com" onChange={handleChange}/>
                 <MultiLineInput value={message} label="Message" name="message" placeholder="Type your message here" onChange={handleChange}/>
