bbb2cb75fee142020514f1fd1e65e4bf1f6df680
Matthew Malone <mama4294@colorado.edu>
2022-05-09 12:32:25 -0600

    adds framer-motion animation to SkillItem component

--- src/components/SkillItem.jsx
+++ src/components/SkillItem.jsx
@@ -1,5 +1,15 @@
+import {motion} from "framer-motion"
+
 export const SkillItem = ({children}) =>  {
     return(
-      <li className="inline-block px-2 py-1 my-1 mr-2 text-primary bg-white rounded-sm">{children}</li>
+      <motion.li 
+      className="inline-block px-2 py-1 my-1 mr-2 text-primary bg-white rounded-sm"
+      initial={{ opacity: 0, x:100 }}
+      whileInView={{ opacity: 1, x:0 }}
+      transition={{type:'spring', duration: 1, bounce: 0.3}}
+      viewport={{ once: true }}
+      >
+        {children}
+      </motion.li>
     )
   }
